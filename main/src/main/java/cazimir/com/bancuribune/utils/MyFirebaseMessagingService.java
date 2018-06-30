7b05c45440f9f080e2a6ae750d86b97a0af09cb2
Cazimir Roman <cazimir.roman@gmail.com>
2018-06-30 13:20:53 +0200

    added channel for notifications, fixed double launcher items

--- main/src/main/java/cazimir/com/bancuribune/utils/MyFirebaseMessagingService.java
+++ main/src/main/java/cazimir/com/bancuribune/utils/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package cazimir.com.bancuribune.utils;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -48,6 +50,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            /* Create or update. */
+            NotificationChannel channel = new NotificationChannel("my_channel_01",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
