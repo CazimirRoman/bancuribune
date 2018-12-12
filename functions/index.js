efa37d394b3eaf65de42bc9eefaa8ffd41c8ad3f
Other name <other.user@email.com>
2018-12-12 12:50:56 +0100

    modified rank text when first rank entered in DB and other text for susequent level-ups

--- functions/index.js
+++ functions/index.js
@@ -13,57 +13,57 @@ exports.onJokeApproved_test = functions.database.ref('/_dev/jokes_dev/{pushId}')
 
         var jokeId = -1;
 
-        if((!event.before.child("approved").val()) && event.after.child("approved").val()){
+        if ((!event.before.child("approved").val()) && event.after.child("approved").val()) {
 
-        const message = event.after.child("jokeText").val();
-                const createdBy = event.after.child("createdBy").val();
-                jokeId = event.after.child("uid").val();
+            const message = event.after.child("jokeText").val();
+            const createdBy = event.after.child("createdBy").val();
+            jokeId = event.after.child("uid").val();
 
-                console.log('The joke ' + message + ' was created by: ' + createdBy + ' and has the id: ' + jokeId);
+            console.log('The joke ' + message + ' was created by: ' + createdBy + ' and has the id: ' + jokeId);
 
-                //first of all get user object based on uid for the created joke
-                const getUser = admin.database().ref("/_dev/users_dev").orderByChild('userId').equalTo(createdBy).once('value')
+            //first of all get user object based on uid for the created joke
+            const getUser = admin.database().ref("/_dev/users_dev").orderByChild('userId').equalTo(createdBy).once('value')
 
-                //return this promise first
-                return Promise.all([getUser]).then(results => {
+            //return this promise first
+            return Promise.all([getUser]).then(results => {
 
-                    var userUid;
+                var userUid;
 
-                    results[0].forEach(function(childSnapshot) {
-                        var value = childSnapshot.val();
-                        userUid = value.uid;
-                        console.log("The key for the user is: " + value.uid)
-                    });
+                results[0].forEach(function(childSnapshot) {
+                    var value = childSnapshot.val();
+                    userUid = value.uid;
+                    console.log("The key for the user is: " + value.uid)
+                });
 
-                    //using the key of the user get access to the instanceId property in the user object
-                    var getInstanceId = admin.database().ref(`/_dev/users_dev/${userUid}/instanceId`).once('value');
+                //using the key of the user get access to the instanceId property in the user object
+                var getInstanceId = admin.database().ref(`/_dev/users_dev/${userUid}/instanceId`).once('value');
 
-                    //then return this promise
-                    return Promise.all([getInstanceId]).then(results => {
+                //then return this promise
+                return Promise.all([getInstanceId]).then(results => {
 
-                        const instanceId = results[0].val();
+                    const instanceId = results[0].val();
 
-                        console.log("The instance id is: " + instanceId)
+                    console.log("The instance id is: " + instanceId)
 
-                        const payload = {
+                    const payload = {
 
-                            data: {
-                                title: "Bancul tău a fost aprobat, felicitări!",
-                                body: "Trimite-l prietenilor, adună voturi și urcă în rang. Click aici să vezi bancul.",
-                                regards: JOKE_APPROVED,
-                                jokeId: jokeId
-                            }
-                        };
+                        data: {
+                            title: "Bancul tău a fost aprobat, felicitări!",
+                            body: "Trimite-l prietenilor, adună voturi și urcă în rang. Click aici să vezi bancul.",
+                            regards: JOKE_APPROVED,
+                            jokeId: jokeId
+                        }
+                    };
 
-                        admin.messaging().sendToDevice(instanceId, payload)
-                            .then(function(response) {
-                                console.log("Successfully sent message:", response);
-                            })
-                            .catch(function(error) {
-                                console.log("Error sending message:", error);
-                            });
-                    })
+                    admin.messaging().sendToDevice(instanceId, payload)
+                        .then(function(response) {
+                            console.log("Successfully sent message:", response);
+                        })
+                        .catch(function(error) {
+                            console.log("Error sending message:", error);
+                        });
                 })
+            })
         }
 
 
@@ -76,110 +76,195 @@ exports.onJokeApproved_prod = functions.database.ref('/jokes/{pushId}')
 
         var jokeId = -1;
 
-        if((!event.before.child("approved").val()) && event.after.child("approved").val()){
+        if ((!event.before.child("approved").val()) && event.after.child("approved").val()) {
 
-        const message = event.after.child("jokeText").val();
-                const createdBy = event.after.child("createdBy").val();
-                jokeId = event.after.child("uid").val();
+            const message = event.after.child("jokeText").val();
+            const createdBy = event.after.child("createdBy").val();
+            jokeId = event.after.child("uid").val();
 
-                console.log('The joke ' + message + ' was created by: ' + createdBy + ' and has the id: ' + jokeId);
+            console.log('The joke ' + message + ' was created by: ' + createdBy + ' and has the id: ' + jokeId);
 
-                //first of all get user object based on uid for the created joke
-                const getUser = admin.database().ref("/users").orderByChild('userId').equalTo(createdBy).once('value')
+            //first of all get user object based on uid for the created joke
+            const getUser = admin.database().ref("/users").orderByChild('userId').equalTo(createdBy).once('value')
 
-                //return this promise first
-                return Promise.all([getUser]).then(results => {
+            //return this promise first
+            return Promise.all([getUser]).then(results => {
 
-                    var userUid;
+                var userUid;
 
-                    results[0].forEach(function(childSnapshot) {
-                        var value = childSnapshot.val();
-                        userUid = value.uid;
-                        console.log("The key for the user is: " + value.uid)
-                    });
+                results[0].forEach(function(childSnapshot) {
+                    var value = childSnapshot.val();
+                    userUid = value.uid;
+                    console.log("The key for the user is: " + value.uid)
+                });
 
-                    //using the key of the user get access to the instanceId property in the user object
-                    var getInstanceId = admin.database().ref(`/users/${userUid}/instanceId`).once('value');
+                //using the key of the user get access to the instanceId property in the user object
+                var getInstanceId = admin.database().ref(`/users/${userUid}/instanceId`).once('value');
 
-                    //then return this promise
-                    return Promise.all([getInstanceId]).then(results => {
+                //then return this promise
+                return Promise.all([getInstanceId]).then(results => {
 
-                        const instanceId = results[0].val();
+                    const instanceId = results[0].val();
 
-                        console.log("The instance id is: " + instanceId)
+                    console.log("The instance id is: " + instanceId)
 
-                        const payload = {
+                    const payload = {
 
-                            data: {
-                                title: "Bancul tău a fost aprobat, felicitări!",
-                                body: "Trimite-l prietenilor, adună voturi și urcă în rang. Click aici să vezi bancul.",
-                                regards: JOKE_APPROVED,
-                                jokeId: jokeId
-                            }
-                        };
+                        data: {
+                            title: "Bancul tău a fost aprobat, felicitări!",
+                            body: "Trimite-l prietenilor, adună voturi și urcă în rang. Click aici să vezi bancul.",
+                            regards: JOKE_APPROVED,
+                            jokeId: jokeId
+                        }
+                    };
 
-                        admin.messaging().sendToDevice(instanceId, payload)
-                            .then(function(response) {
-                                console.log("Successfully sent message:", response);
-                            })
-                            .catch(function(error) {
-                                console.log("Error sending message:", error);
-                            });
-                    })
+                    admin.messaging().sendToDevice(instanceId, payload)
+                        .then(function(response) {
+                            console.log("Successfully sent message:", response);
+                        })
+                        .catch(function(error) {
+                            console.log("Error sending message:", error);
+                        });
                 })
+            })
         }
 
     });
 
-    exports.onRankUpdated_test = functions.database.ref('/_dev/ranks_dev/{pushId}')
-        .onWrite(event => {
+exports.onRankUpdated_test = functions.database.ref('/_dev/ranks_dev/{pushId}')
+    .onWrite(event => {
 
-            var jokeId = -1;
-            var instanceId = null;
-            var createdBy = event.after.child("userId").val();
-            var currentRank = event.after.child("rank").val();
+        var jokeId = -1;
+        var instanceId = null;
+        var createdBy = event.after.child("userId").val();
+        var currentRank = event.after.child("rank").val();
 
-                //first of all get user object based on uid for the created joke
-                const getUser = admin.database().ref("/_dev/users_dev").orderByChild('userId').equalTo(createdBy).once('value')
+        //first of all get user object based on uid for the created joke
+        const getUser = admin.database().ref("/_dev/users_dev").orderByChild('userId').equalTo(createdBy).once('value')
 
-                //return this promise first
-                return Promise.all([getUser]).then(results => {
+        //return this promise first
+        return Promise.all([getUser]).then(results => {
 
-                    var userUid;
+            var userUid;
 
-                    results[0].forEach(function(childSnapshot) {
-                        var value = childSnapshot.val();
-                        userUid = value.uid;
-                        console.log("The key for the user is: " + value.uid)
-                    });
+            results[0].forEach(function(childSnapshot) {
+                var value = childSnapshot.val();
+                userUid = value.uid;
+                console.log("The key for the user is: " + value.uid)
+            });
 
-                    //using the key of the user get access to the instanceId property in the user object
-                    var getInstanceId = admin.database().ref(`/_dev/users_dev/${userUid}/instanceId`).once('value');
+            //using the key of the user get access to the instanceId property in the user object
+            var getInstanceId = admin.database().ref(`/_dev/users_dev/${userUid}/instanceId`).once('value');
 
-                    //then return this promise
-                    return Promise.all([getInstanceId]).then(results => {
+            //then return this promise
+            return Promise.all([getInstanceId]).then(results => {
 
-                        const instanceId = results[0].val();
+                const instanceId = results[0].val();
 
-                        console.log("The instance id is: " + instanceId)
+                console.log("The instance id is: " + instanceId)
 
-                        const payload = {
+                if (currentRank === 'Hamsie') {
 
-                            data: {
-                                title: `Ai urcat în rangul pestilor, felicitari!`,
-                                body: `Acum ești ${currentRank}. \nAdaugă bancuri, adună voturi și trimite-le prietenilor că să ajungi un pește mai mare. Verifică aici numărul de voturi până la următorul rang în bancul de pești.`,
-                                regards: RANK_UPDATED
-                            }
-                        };
+                    var payload = {
 
-                        admin.messaging().sendToDevice(instanceId, payload)
-                            .then(function(response) {
-                                console.log("Successfully sent message:", response);
-                            })
-                            .catch(function(error) {
-                                console.log("Error sending message:", error);
-                            });
+                        data: {
+                            title: `În bancul de pești ești ${currentRank}.`,
+                            body: `Adaugă bancuri, adună voturi și trimite-le prietenilor că să ajungi un pește mai mare. Verifică aici numărul de voturi până la următorul rang în bancul de pești.`,
+                            regards: RANK_UPDATED
+                        }
+
+                    };
+
+                } else {
+
+                    var payload = {
+
+                        data: {
+                            title: `Ai urcat în rangul peștilor, felicitări!`,
+                            body: `Acum ești ${currentRank}. \nAdaugă bancuri, adună voturi și trimite-le prietenilor ca să ajungi un pește mai mare. Verifică aici numărul de voturi până la următorul rang în bancul de pești.`,
+                            regards: RANK_UPDATED
+                        }
+
+                    };
+
+                }
+
+                admin.messaging().sendToDevice(instanceId, payload)
+                    .then(function(response) {
+                        console.log("Successfully sent message:", response);
                     })
-                })
+                    .catch(function(error) {
+                        console.log("Error sending message:", error);
+                    });
+            })
+        })
 
-        });
+    });
+//wait for last version to be on the store.
+
+//        exports.onRankUpdated_prod = functions.database.ref('/ranks/{pushId}')
+//                .onWrite(event => {
+//
+//                    var jokeId = -1;
+//                    var instanceId = null;
+//                    var createdBy = event.after.child("userId").val();
+//                    var currentRank = event.after.child("rank").val();
+//
+//                        //first of all get user object based on uid for the created joke
+//                        const getUser = admin.database().ref("/users").orderByChild('userId').equalTo(createdBy).once('value')
+//
+//                        //return this promise first
+//                        return Promise.all([getUser]).then(results => {
+//
+//                            var userUid;
+//
+//                            results[0].forEach(function(childSnapshot) {
+//                                var value = childSnapshot.val();
+//                                userUid = value.uid;
+//                                console.log("The key for the user is: " + value.uid)
+//                            });
+//
+//                            //using the key of the user get access to the instanceId property in the user object
+//                            var getInstanceId = admin.database().ref(`/users/${userUid}/instanceId`).once('value');
+//
+//                            //then return this promise
+//                            return Promise.all([getInstanceId]).then(results => {
+//
+//                                const instanceId = results[0].val();
+//
+//                                console.log("The instance id is: " + instanceId)
+//
+//                                const payload = {
+//
+//                                    if(currentRank === 'Hamsie'){
+//
+//                                    data: {
+//                                        title: `În bancul de pești ești ${currentRank}.`,
+//                                        body: `Adaugă bancuri, adună voturi și trimite-le prietenilor că să ajungi un pește mai mare. Verifică aici numărul de voturi până la următorul rang în bancul de pești.`,
+//                                        regards: RANK_UPDATED
+//                                    }
+//
+//                                    }else{
+//
+//                                    data: {
+//                                        title: `Ai urcat în rangul peștilor, felicitari!`,
+//                                        body: `Acum ești ${currentRank}. \nAdaugă bancuri, adună voturi și trimite-le prietenilor că să ajungi un pește mai mare. Verifică aici numărul de voturi până la următorul rang în bancul de pești.`,
+//                                        regards: RANK_UPDATED
+//                                    }
+//
+//                                    }
+//
+//
+//                                };
+//
+//                                admin.messaging().sendToDevice(instanceId, payload)
+//                                    .then(function(response) {
+//                                        console.log("Successfully sent message:", response);
+//                                    })
+//                                    .catch(function(error) {
+//                                        console.log("Error sending message:", error);
+//                                    });
+//                            })
+//                        })
+//
+//                });
