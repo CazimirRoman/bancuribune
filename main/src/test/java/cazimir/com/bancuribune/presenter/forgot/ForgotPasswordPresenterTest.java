239a9b4e124407ed58a50fe147d88a4abfaba1c5
Roman Cazimir <extern.Cazimir.Roman@elektrobit.com>
2019-01-02 16:26:28 +0100

    added unit tests for forgot password presenter

---
+++ main/src/test/java/cazimir/com/bancuribune/presenter/forgot/ForgotPasswordPresenterTest.java
@@ -0,0 +1,79 @@
+package cazimir.com.bancuribune.presenter.forgot;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import cazimir.com.bancuribune.presenter.auth.AuthPresenter;
+import cazimir.com.bancuribune.view.forgotPassword.ForgotPasswordActivityView;
+import cazimir.com.bancuribune.view.forgotPassword.OnResendVerificationEmailListener;
+import cazimir.com.bancuribune.view.forgotPassword.OnResetPasswordListener;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+/**
+ * TODO: Add a class header comment!
+ */
+public class ForgotPasswordPresenterTest {
+
+    private static final String EMAIL = "email@gmail.com";
+    private static final String PASSWORD = "123456";
+    private static final String RESENT_EMAIL_SUCCEEDED = "Email resend succeeded";
+    private static final String RESEND_EMAIL_FAILED = "Email resend failed";
+
+    private ForgotPasswordPresenter mForgotPasswordPresenter;
+
+    @Mock
+    private ForgotPasswordActivityView mForgotPasswordActivityView;
+
+    @Mock
+    private AuthPresenter mAuthPresenter;
+
+    @Captor
+    private ArgumentCaptor<OnResendVerificationEmailListener> mOnResendVerificationEmailListenerArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<OnResetPasswordListener> mOnResetPasswordListenerArgumentCaptor;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mForgotPasswordPresenter = new ForgotPasswordPresenter(mForgotPasswordActivityView, mAuthPresenter);
+    }
+
+    @Test
+    public void shouldRedirectToLoginIfActivationEmailResendSuccess() {
+        mForgotPasswordPresenter.resendVerificationEmail(EMAIL, PASSWORD);
+        verify(mAuthPresenter).performResendVerificationEmail(mOnResendVerificationEmailListenerArgumentCaptor.capture(), eq(EMAIL), eq(PASSWORD));
+        mOnResendVerificationEmailListenerArgumentCaptor.getValue().onResendEmailSuccess(RESENT_EMAIL_SUCCEEDED);
+        InOrder inOrder = Mockito.inOrder(mForgotPasswordActivityView);
+        inOrder.verify(mForgotPasswordActivityView).showToast(RESENT_EMAIL_SUCCEEDED);
+        inOrder.verify(mForgotPasswordActivityView).redirectToLogin();
+        inOrder.verify(mForgotPasswordActivityView).hideProgress();
+    }
+
+    @Test
+    public void shouldShowErrorToastIfActivationEmailResendFailed() {
+        mForgotPasswordPresenter.resendVerificationEmail(EMAIL, PASSWORD);
+        verify(mAuthPresenter).performResendVerificationEmail(mOnResendVerificationEmailListenerArgumentCaptor.capture(), eq(EMAIL), eq(PASSWORD));
+        mOnResendVerificationEmailListenerArgumentCaptor.getValue().onResendEmailFailed(RESEND_EMAIL_FAILED);
+        InOrder inOrder = Mockito.inOrder(mForgotPasswordActivityView);
+        inOrder.verify(mForgotPasswordActivityView).hideProgress();
+        inOrder.verify(mForgotPasswordActivityView).showToast(RESEND_EMAIL_FAILED);
+    }
+
+    @Test
+    public void sendResetInstructions() {
+        mForgotPasswordPresenter.sendResetInstructions(EMAIL);
+        verify(mAuthPresenter).performPasswordReset(mOnResetPasswordListenerArgumentCaptor);
+
+
+
+    }
+}
